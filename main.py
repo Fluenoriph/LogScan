c68b99df3cdfce17a064f3a4416f23c99081f79e
Fluenoriph <fluenoriph@gmail.com>
2026-02-25 21:37:54 +1000

    Далее

--- main.py
+++ main.py
@@ -2,28 +2,18 @@
 Название утилиты: Log Scaner
 Версия: 0.9
 Дата: Февраль 2026 г.
-Лицензия: MIT License
-Автор: Богданов Иван Иванович
+Автор: Богданов Иван
 Контакты: fluenoriph@gmail.com, fluenoriph@yandex.ru
 """
 
 import os.path
 import glob
-
 from modules.argument_parser import ArgumentParser
 from modules.program_process import ProgramProcess
 
 
 if __name__ == '__main__':
     argument_parser = ArgumentParser()
-
-    #key = input("Enter your key:")
-    #result_format = input("Enter your format:")
-
-    #source_log = os.path.abspath(r'logs')
-    #out_path = os.path.abspath(r'output')
-
-
 
     if os.path.isfile(argument_parser.arguments.logfile):
         app = ProgramProcess(argument_parser.arguments.logfile,
