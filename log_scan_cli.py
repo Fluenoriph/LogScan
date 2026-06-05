d262b3732173c4980841050715244cf08d1a22d2
Fluenoriph <fluenoriph@gmail.com>
2026-06-06 04:18:27 +1000

    Reload

--- log_scan_cli.py
+++ log_scan_cli.py
@@ -1,14 +1,12 @@
 """
-Название утилиты: Log Scanner
-Версия: 0.9
-Дата: Февраль 2026 г.
-Автор: Богданов Иван
-Контакты: fluenoriph@gmail.com, fluenoriph@yandex.ru
+Program name: Log Scanner
+Version: 0.9
+Date: February 2026 г.
+Author: Ivan Bogdanov
+Contacts: fluenoriph@gmail.com, fluenoriph@yandex.ru
 """
 
-import platform
-import os.path
-import sys
+import os
 import glob
 from modules.argument_parser import ArgumentParser
 from modules.program_process import ProgramProcess
@@ -23,17 +21,9 @@ if os.path.isfile(argument_parser.arguments.logfile):
                          argument_parser.arguments.format)
 
 elif os.path.isdir(argument_parser.arguments.logfile):
-    match platform.system():
-        case "Linux":
-            SLASH = '/'
-        case "Windows":
-            SLASH = '\\'
-        case _:
-            sys.exit()
-
-    files = (glob.glob(rf'{argument_parser.arguments.logfile}{SLASH}*.log') +
-             glob.glob(rf'{argument_parser.arguments.logfile}{SLASH}*.txt'))
+    files = (glob.glob(rf'{argument_parser.arguments.logfile}/*.log') +
+             glob.glob(rf'{argument_parser.arguments.logfile}/*.txt'))
 
     for file in files:
-        app = ProgramProcess(file, argument_parser.arguments.apikey,
+        app = ProgramProcess(os.path.normcase(file), argument_parser.arguments.apikey,
                              argument_parser.arguments.output, argument_parser.arguments.format)
